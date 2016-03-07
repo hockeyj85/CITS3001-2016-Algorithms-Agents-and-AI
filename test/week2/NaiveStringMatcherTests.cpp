83b467307e96d9e0ebdb269cd79ddb9606209e56
James Hockey <hockeyj85@gmail.com>
2016-03-08 00:10:50 +0800

    Add some string matcher tests to stress the algorithm

--- test/week2/NaiveStringMatcherTests.cpp
+++ test/week2/NaiveStringMatcherTests.cpp
@@ -25,3 +25,68 @@ TEST(NaiveMatcherTest, MatchOffBeginning) {
     EXPECT_EQ(-1, result);
 }
 
+/**
+ * Run string matcher on a string that will never match
+ * expects to return -1.
+ */
+int runNaiveMatcher(int textLength, int patternLength) {
+    char *str = new char[textLength];
+    for (int i = 0; i < textLength; i++) {
+        // Use lowercase letters
+        str[i] = (char) (i % ('z' - 'a')) + 'a';
+    }
+    char *pat = new char[patternLength];
+    for (int i = 0; i < patternLength; i++) {
+        // Use lowercase letters
+        pat[i] = (char) (i % ('z' - 'a')) + 'a';
+    }
+    pat[patternLength - 1] = 'A';
+
+    return naiveStringMatcher(pat, str);
+}
+
+
+TEST(NaiveMatcherTest, Len1000String) {
+    int result = runNaiveMatcher(1000, 100);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len10000String) {
+    int result = runNaiveMatcher(10000, 100);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len100000String) {
+    int result = runNaiveMatcher(100000, 1000);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len1000000String) {
+    int result = runNaiveMatcher(1000000, 1000);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len10000000String_smallPattern) {
+    int result = runNaiveMatcher(10000000, 10);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len10000000String_medPattern) {
+    int result = runNaiveMatcher(10000000, 100);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len10000000String_largePattern) {
+    int result = runNaiveMatcher(10000000, 10000);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len100000000String_smallPattern) {
+    int result = runNaiveMatcher(100000000, 10);
+    EXPECT_EQ(-1, result);
+}
+
+TEST(NaiveMatcherTest, Len100000000String_medPattern) {
+    int result = runNaiveMatcher(100000000, 100);
+    EXPECT_EQ(-1, result);
+}
